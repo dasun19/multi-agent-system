be899e524f591fe327b47892e668900e0e1ce777
Dasun Cooray <dasuncooray48@gmail.com>
2025-02-26 19:50:25 +0530

    update the code of multi_agent_system.py file

--- Multi_Agent_System.py
+++ Multi_Agent_System.py
@@ -7,36 +7,21 @@ from colorama import Fore, init
 init(autoreset=True)
 
 
-details_list = []
-total_cost = 0
+def student_reg_agent():
 
-def student_reg_agent(run_last = False):
+    while True:
+        try:
+            student_details = input("\nStudent Registration Agent: Please enter your name and student id(separate with a comma): ").strip()
+            name, student_id = map(str.strip, student_details.split(","))
 
-    global details_list
+            print(f"\nStudent Registration Agent: Welcome, {name.capitalize()}(ID:{student_id}). Proceeding to course selection. ")
+            return name, student_id
 
-    try:
-        if run_last == False:
-            student_details = input("\nStudent Registration Agent: Please enter your name and student id(separate with a comma): ").lower()
-            details = student_details.split(",")
-            details_list = [detail.strip() for detail in details]
-            print(f"\nStudent Registration Agent: Welcome, {details_list[0].capitalize()}(ID:{details_list[1]}). Proceeding to course selection. ")
+        except ValueError:
+            print(Fore.RED + "\nInvalid input. Please enter your name and student id separate from a comma.")
 
-        elif run_last == True:
-            print(f"\nStudent Registration Agent: Your registration is complete. Thank you for registering, {details_list[0].capitalize()}!")
 
-    except IndexError:
-        print(Fore.RED + "\nPlease enter your name and student id separate from a comma.")
-        student_reg_agent()
-
-def course_selection_agent():
-
-    global total_cost
-
-    courses = {
-        'data structures' : 300,
-        'algorithms' : 350,
-        'machine learning' : 400
-    }
+def course_selection_agent(courses):
 
     print("\nCourse Selection Agent: Available courses are: \n")
 
@@ -45,60 +30,76 @@ def course_selection_agent():
         print(f"{i}. {key.capitalize()} - ${value}")
         i+=1
 
-    try:
-        courses_for_reg = input("\nPlease type the courses you want to register for(separate by commas): ").lower()
-        courses_list1 = courses_for_reg.split(',')
-        courses_list = [course.strip() for course in courses_list1]
+    while True:
+
+        try:
+            courses_for_reg = input("\nPlease type the courses you want to register for(separate by commas): ").lower()
+            courses_list1 = courses_for_reg.split(',')
+            courses_list = [course.strip() for course in courses_list1]
+
+            if not all(key in courses for key in courses_list):
+                raise KeyError
+
+            total_cost = 0
+            for key in courses_list:
+                total_cost += courses[key]
+
+            print(f"\nCourse Selection Agent: You have selected {', '.join(courses_list).capitalize()}. The total cost is ${total_cost}.")
+            return total_cost
+
+        except KeyError:
+            print(Fore.RED + "\nPlease enter available courses only and Please type the correct course name as display in the course list.")
 
 
-        for key in courses_list:
-            total_cost += courses[key]
+def payment_agent(total_cost):
 
-        print(f"\nCourse Selection Agent: You have selected {', '.join(courses_list).capitalize()}. The total cost is ${total_cost}.")
+    while True:
+        try:
+            payment = int(input("\nPayment Agent: Please enter your payment amount($): ").strip())
 
-    except KeyError:
-        print(Fore.RED + "\nPlease enter available courses only and Please type the correct course name as display in the course list.")
-        course_selection_agent()
+            if payment == total_cost:
+                print(f"\nPayment Agent: Payment of ${payment} received. Your registration is confirmed.")
+                return
 
-def payment_agent():
+            else:
+                print(Fore.RED + "\nPlease enter the correct total cost.")
 
-    global total_cost
-
-    payment = int(input("\nPayment Agent: Please enter your payment amount($): ").strip())
-
-    try:
-
-        if payment == total_cost:
-            print(f"\nPayment Agent: Payment of ${payment} received. Your registration is confirmed.")
-            student_reg_agent(run_last=True)
-
-        else:
-            print(Fore.RED + "\nPlease enter the correct total cost.")
-            payment_agent()
-    except ValueError:
-        print(Fore.RED + "\nPlease enter the correct total cost."  )
-        payment_agent()
+        except ValueError:
+            print(Fore.RED + "\nPlease enter the correct total cost."  )
 
 
-if __name__ == "__main__":
+
+def main():
+
+    courses = {
+        'data structures': 300,
+        'algorithms': 350,
+        'machine learning': 400
+    }
 
     while True:
         print("\n\n.....University Student Registration System.....")
         print("\nOptions: ")
         print("\n1 - I want to register for courses.")
         print("2 - Exit from the university registration system.")
-        options = ['1', '2']
+
 
         option = input("\nSelect an Option(1 or 2): ")
 
-        if option not in options:
-            print(Fore.RED + "\nPlease select an valid option.")
 
-        elif option == '1':
-            student_reg_agent()
-            course_selection_agent()
-            payment_agent()
+        if option == '1':
+            name, student_id = student_reg_agent()
+            total_cost = course_selection_agent(courses)
+            payment_agent(total_cost)
+            print(f"\nStudent Registration Agent: Your registration is complete. Thank you for registering, {name.capitalize()}!")
 
         elif option == '2':
             print("\nYou are exiting from the registration system...")
             break
+
+        else:
+            print(Fore.RED + "\nPlease select an valid option(1 or 2).")
+
+
+if __name__ == "__main__":
+    main()
